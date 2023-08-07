279c2d42da4b7dc9c8421383c06ad5f72431a4e5
Margaret Camilletti <mhcamilletti@gmail.com>
2023-08-06 21:15:04 -0400

    added github icon to footer

--- Margaret-Camilletti-Portfolio/src/components/Footer.jsx
+++ Margaret-Camilletti-Portfolio/src/components/Footer.jsx
@@ -4,9 +4,7 @@ import { Link } from 'react-router-dom'
 const Footer = () => {
     return (
        <footer className="app-footer">
-        <Link className="nav" to="home" id="home-link" smooth={true} duration={500}>
-          Margaret Camilletti
-        </Link>
+        <p><i class="uil uil-github"></i></p>
        </footer> 
     )
 }
