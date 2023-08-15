68675546527fc6ca3a083a6f958400fa684f6af7
Margaret Camilletti <mhcamilletti@gmail.com>
2023-08-15 13:55:56 -0400

    added github icon

--- Margaret-Camilletti-Portfolio/src/components/Footer.jsx
+++ Margaret-Camilletti-Portfolio/src/components/Footer.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 const Footer = () => {
     return (
        <footer className="app-footer">
-        <p><i className="uil uil-github"></i></p>
+        <p><image className="icon" src="src/assets/pngaaa.com-94893.png"></image></p>
        </footer> 
     )
 }
