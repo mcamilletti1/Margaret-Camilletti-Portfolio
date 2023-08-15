ff3266cbb367c03660a1caded3d515e81fabff2d
Margaret Camilletti <mhcamilletti@gmail.com>
2023-08-15 13:21:47 -0400

    updated projects description

--- Margaret-Camilletti-Portfolio/src/components/Footer.jsx
+++ Margaret-Camilletti-Portfolio/src/components/Footer.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 const Footer = () => {
     return (
        <footer className="app-footer">
-        <p><i class="uil uil-github"></i></p>
+        <p><i className="uil uil-github"></i></p>
        </footer> 
     )
 }
