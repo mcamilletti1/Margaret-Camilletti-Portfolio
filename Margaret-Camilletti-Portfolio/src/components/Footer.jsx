b6608d54e6997e199ffd9cfea0c7f8533f9dda24
Margaret Camilletti <mhcamilletti@gmail.com>
2023-08-15 16:38:17 -0400

    updated footer

--- Margaret-Camilletti-Portfolio/src/components/Footer.jsx
+++ Margaret-Camilletti-Portfolio/src/components/Footer.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 const Footer = () => {
     return (
        <footer className="app-footer">
-        <p><image className="icon" src="src/assets/pngaaa.com-94893.png"></image></p>
+        <image className="icon" src="src/assets/pngaaa.com-94893.png"></image>
        </footer> 
     )
 }
