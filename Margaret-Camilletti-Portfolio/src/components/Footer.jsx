7cdb6c9baf55993646513bc4061586425108de5b
Margaret Camilletti <mhcamilletti@gmail.com>
2023-08-15 16:48:30 -0400

    fixed error in footer

--- Margaret-Camilletti-Portfolio/src/components/Footer.jsx
+++ Margaret-Camilletti-Portfolio/src/components/Footer.jsx
@@ -1,10 +1,10 @@
-import { Link } from 'react-router-dom'
+
 
 
 const Footer = () => {
     return (
        <footer className="app-footer">
-        <image className="icon" src="src/assets/pngaaa.com-94893.png"></image>
+        <img className="icon" src="src/assets/pngaaa.com-94893.png"></img>
        </footer> 
     )
 }
