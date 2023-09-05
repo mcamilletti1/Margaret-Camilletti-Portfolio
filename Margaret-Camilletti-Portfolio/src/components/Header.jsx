eb15a427f4967efe79784bc1f7d5504dc8753b7d
Margaret Camilletti <mhcamilletti@gmail.com>
2023-09-05 12:19:02 -0400

    made socials links and hamburger menu more accessible

--- Margaret-Camilletti-Portfolio/src/components/Header.jsx
+++ Margaret-Camilletti-Portfolio/src/components/Header.jsx
@@ -9,17 +9,31 @@ const Header = () => {
     setShowMenu(!showMenu);
   };
 
+  const handleMenuKeyDown = (e) => {
+    if (e.key === 'Enter' || e.key === 'Space') {
+      handleMenuToggle();
+    }
+  };
+
   return (
     <header className="header-container">
       <div className="left-side">
-        <Link className="nav" to="home" id="home-link" smooth={true} duration={500}>
-          Margaret Camilletti
+        <Link className="nav" to="/home" id="home-link" smooth={true} duration={500}>
+          <span className="sr-only">Margaret Camilletti</span>
         </Link>
       </div>
       <div className="right-side">
-      <a target="_blank" href="https://github.com/mcamilletti1"><img height="30px" width="30px" className="icon" src="src/assets/pngaaa.com-94893.png"></img></a>
-      <a target="_blank" href="https://linkedin.com/in/margaretcamilletti"><img height="30px" width="30px" className="icon" src="src/assets/kisspng-united-states-hilton-hotels-resorts-organization-linkedin-white-5b39721a0b09e5.9500994915304914180452.png"></img></a>
-        <div className={`hamburger-menu ${showMenu ? 'active' : ''}`} onClick={handleMenuToggle}>
+      <a target="_blank" rel="noopener noreferrer" href="https://github.com/mcamilletti1"><img height="30px" width="30px" className="icon" src="src/assets/pngaaa.com-94893.png" alt="GitHub"></img></a>
+      <a target="_blank" rel="noopener noreferrer" href="https://linkedin.com/in/margaretcamilletti"><img height="30px" width="30px" className="icon" src="src/assets/kisspng-united-states-hilton-hotels-resorts-organization-linkedin-white-5b39721a0b09e5.9500994915304914180452.png" alt="LinkedIn"></img></a>
+        <div className={`hamburger-menu ${showMenu ? 'active' : ''}`} 
+        onClick={handleMenuToggle}
+        onKeyDown={handleMenuKeyDown}
+        tabIndex={0}
+        role="button"
+        aria-label="Toggle Menu"
+        aria-expanded={showMenu}
+        aria-controls="menu-content"
+        >
           <div className="bar"></div>
           <div className="bar"></div>
           <div className="bar"></div>
