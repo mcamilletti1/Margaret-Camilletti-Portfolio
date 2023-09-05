59e7c2cc8b37f7f39c9272bcfc4743b1e7757ebd
Margaret Camilletti <mhcamilletti@gmail.com>
2023-09-05 12:24:58 -0400

    improved accessibility for home component

--- Margaret-Camilletti-Portfolio/src/components/Home.jsx
+++ Margaret-Camilletti-Portfolio/src/components/Home.jsx
@@ -3,10 +3,10 @@
 const Home = () => {
     return (
         <section id="home" className="homePage">
-            <img id="headshot" src="https://avatars.githubusercontent.com/u/125098092?s=400&u=aa0c7cab240a30e29e941cd98340d07605ba5174&v=4"/>
+            <img id="headshot" src="https://avatars.githubusercontent.com/u/125098092?s=400&u=aa0c7cab240a30e29e941cd98340d07605ba5174&v=4" alt="Margaret's headshot"/>
             <section className="subheading">
                 <h1 id="name">Hi, I'm Margaret.</h1>
-                <h3 id="homeH3">a Software Developer based in NYC.</h3>
+                <h2 id="homeH2">a Software Developer based in NYC.</h2>
             </section>
         </section>
     )
