33fd968bc34e45b0998559aa9238cd5079551d21
Margaret Camilletti <mhcamilletti@gmail.com>
2023-11-02 16:32:54 -0400

    updated resume section of my portfolio

--- Margaret-Camilletti-Portfolio/src/components/Resume.jsx
+++ Margaret-Camilletti-Portfolio/src/components/Resume.jsx
@@ -6,32 +6,27 @@ const Resume = () => {
             <a className="resume-links" id="open-resume" href="https://docs.google.com/document/d/1h6uCClB465Wmb6NCpRFIqEmKK_jax3MuAEbS4NlPcFs/edit?usp=sharing" rel="noopener noreferrer" target="_blank"><h3>Link to Resume</h3></a><br></br>
  <p>
 <strong>SKILLS<br></br><br></br>
-Languages |</strong> JavaScript, HTML, CSS, Python, TypeScript<br></br>
-<strong>Libraries and Frameworks |</strong> React, Node.js, Express.js, Django, Flask, Angular<br></br>
+Languages |</strong> JavaScript, HTML, CSS, Python, TypeScript, Go<br></br>
+<strong>Libraries and Frameworks |</strong> React, React Native, Node.js, Express.js, Django, Flask, Angular<br></br>
 <strong>Database |</strong> PostgreSQL, Sequelize, ORM, MongoDB, Mongoose, ODM, ElephantSQL, MongoDBAtlas<br></br>
-<strong>Other |</strong> RESTful Routing JSON, MERN stack, MEAN stack, ChatGPT, Canva, Figma, Postman, ThunderClient<br></br><br></br>
+<strong>Other |</strong> RESTful Routing JSON, MERN stack, MEAN stack, ChatGPT, Canva, Figma, Postman, ThunderClient, Adobe Photoshop, Wordpress<br></br><br></br>
 
 
 <strong>PROFESSIONAL EXPERIENCE<br></br>
+
+Backend Developer | Revent Solutions | Remote 11/2023-Present</strong><br></br>
+<ul><li>Worked on backend team of Revent Solutions to build robust, secure, and scalable web applications</li>
+<li>Created tailored solutions based on insightful data.</li></ul><br></br><br></br>
+
 Vice President | Squeaky Wheel Media 501(c)(3) | New York, NY	5/2021-Present</strong><br></br>
 <ul><li>Co-founded non-profit The Squeaky Wheel, a satirical publication that aims to spread awareness about disability rights issues through humor.</li>
 <li>Write and edit articles for website, promote accessibility and disability rights issues through publicity events, provide support for team of satire writers</li></ul><br></br><br></br>
-
-<strong>Social Media/Digital Content Intern | Fusion Theatre Company | New York, NY	9/2020-08/2021</strong>
-<ul><li>Posted and ran livestreams on social media accounts, made updates and changes to Wix website, designed and created posters and promotional content using Canva for upcoming shows and fundraisers</li>
-<li>Designed and sent promotional emails to clients and sponsors using MailChimp</li></ul><br></br><br></br>
-
-<strong>Ticket Agent | TodayTix | New York, NY</strong><br></br>
-<ul><li>Purchased tickets, entered data, and organized ticket inventory for TodayTix, a Broadway ticket company</li>
-<li>Provided excellent customer service for customers, including accommodating for customers’ specific accessibility needs</li>
-<li>Stayed up-to-date on constantly-changing news, rules, and guidelines related to the Broadway community and Broadway ticket sales to better assist customers</li>
-<li>Maintained consistent work ethic in fast-paced and demanding work environment</li></ul><br></br><br></br>
 
 <strong>EDUCATION<br></br>
 General Assembly|</strong> Software Engineering Immersive				 	  	8/2023<br></br>
 Full-stack software engineering immersive student in an intensive, 450+ hour program focused on product development fundamentals, object-oriented programming, MVC frameworks, data modeling, and team collaboration strategies. Developed a portfolio of individual and group projects.<br></br><br></br>
 
-<strong>Marymount Manhattan College |</strong> Bachelor’s of Fine Arts | New York, NY				5/2022
+<strong>Marymount Manhattan College |</strong> Bachelor’s Degree | New York, NY				5/2022
 
 
 </p>
