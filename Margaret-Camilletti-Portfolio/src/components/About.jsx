1d71c42897630b4a016006c782aaff6cd4fa16a6
Margaret Camilletti <mhcamilletti@gmail.com>
2023-11-14 15:23:39 -0500

    updated about me section

--- Margaret-Camilletti-Portfolio/src/components/About.jsx
+++ Margaret-Camilletti-Portfolio/src/components/About.jsx
@@ -4,7 +4,7 @@ const About = () => {
     return (
         <section id="about" aria-labelledby="about-header">
             <h3 id="about-header">About Me</h3>
-            <p id="about-paragraph">Hi there! My name is Margaret Camilletti and I am a full-stack software developer based in Manhattan, New York. As a former theatre artist turned software engineer, I am passionate about merging creativity and tech, and a proud member of #ArtistsWhoCode. As a disability rights advocate, I strive to make all of my web applications accessible, as well as stylish. Above all else, I believe in cultivating empathy in all that I do.</p>
+            <p id="about-paragraph">As a passionate and innovative Software Developer based in New York, NY, I bring a unique blend of technical expertise, creative problem-solving, and a user-centric approach to software development. With a Bachelor's degree from Marymount Manhattan College and intensive training in Software Engineering from General Assembly, I have honed my skills to create software solutions that are not only functional but also aesthetically pleasing and intuitive.</p>
         </section>
     )
 } 
