2d4c68e1745752a3968aacbfa48cde333980aeb3
Margaret Camilletti <mhcamilletti@gmail.com>
2023-11-16 15:06:00 -0500

    updated projects and resume

--- Margaret-Camilletti-Portfolio/src/components/Projects.jsx
+++ Margaret-Camilletti-Portfolio/src/components/Projects.jsx
@@ -6,14 +6,14 @@ const Projects = () => {
             <h3 id="projects-header">Recent Projects</h3>
             <section id="project-links">
                 <section id="project1">
-                    <a href="https://walksafenyc.netlify.app/" target="_blank" rel="noopener noreferrer"><img className="project-image" src="src/assets/Screenshot 2023-07-21 at 2.35.15 PM.png" alt="WalkSafe NYC Project Screenshot"/></a>
-                    <h4><a href="https://walksafenyc.netlify.app/" className="project-title" target="_blank" rel="noopener noreferrer">WalkSafe NYC</a></h4>
+                    <a href="https://calm-candle-shop.onrender.com/" target="_blank" rel="noopener noreferrer"><img className="project-image" src="src/assets/Screenshot 2023-08-04 at 5.09.14 PM (2).png"></img></a>
+                    <h4><a href="https://calm-candle-shop.onrender.com/" className="project-title" target="_blank" rel="noopener noreferrer">Calm Candle Shop</a></h4>
                     <div className="project-description">
-                        <p>Full-Stack React Application</p>
-                        <p>HTML, CSS, JavaScript, React, MongoDB, Mongoose  	07/2023</p>
+                        <p>Full-Stack MEAN Application</p>
+                        <p>HTML, CSS, TypeScript, Angular, MongoDB, Mongoose    08/2023</p>
                     <ul>
-                        <li>Worked on a team of four developers to create a full-stack mobile-optimized application using React that maps and tracks instances of street harassment and catcalling in New York City so you can walk safely through the five boroughs.</li>
-                        <li>Set up and tested backend CRUD using Thunderclient and frontend CRUD for user accounts and reports of harassment, set up React components and routes, created all React forms and set up user authentication, set up persistent login functionality</li>
+                        <li>Built frontend in Angular and TypeScript</li>
+                        <li>Backend API built using MongoDB, Mongoose, Express, and Node JS</li>
                     </ul>
                     </div>
                 </section>
@@ -32,6 +32,18 @@ const Projects = () => {
                     </div>
                 </section>
                 <section id="project3">
+                    <a href="https://walksafenyc.netlify.app/" target="_blank" rel="noopener noreferrer"><img className="project-image" src="src/assets/Screenshot 2023-07-21 at 2.35.15 PM.png" alt="WalkSafe NYC Project Screenshot"/></a>
+                    <h4><a href="https://walksafenyc.netlify.app/" className="project-title" target="_blank" rel="noopener noreferrer">WalkSafe NYC</a></h4>
+                    <div className="project-description">
+                        <p>Full-Stack React Application</p>
+                        <p>HTML, CSS, JavaScript, React, MongoDB, Mongoose  	07/2023</p>
+                    <ul>
+                        <li>Worked on a team of four developers to create a full-stack mobile-optimized application using React that maps and tracks instances of street harassment and catcalling in New York City so you can walk safely through the five boroughs.</li>
+                        <li>Set up and tested backend CRUD using Thunderclient and frontend CRUD for user accounts and reports of harassment, set up React components and routes, created all React forms and set up user authentication, set up persistent login functionality</li>
+                    </ul>
+                    </div>
+                </section>
+                <section id="project4">
                     <a href="https://art-trivia-game.io/" target="_blank" rel="noopener noreferrer">
                         <img className="project-image" src="src/assets/Screenshot 2023-07-23 at 7.40.55 PM.png" alt="Art Trivia Project Screenshot" /></a>
                     <h4><a href="https://art-trivia-game.io/" className="project-title" target="_blank" rel="noopener noreferrer">Guess the Artist</a></h4>
@@ -42,18 +54,6 @@ const Projects = () => {
                         <li>Utilized the Art Institute of Chicago’s API, Axios, and Vanilla JavaScript to create a trivia game where users have to guess the artist based on the painting.</li>
                         <li>Created game logic in JavaScript and pulled data from the API using Axios to populate multiple choice questions and information about each painting, included randomizing multiple choice functionality and score counter</li>
                         <li>Created a professionally-styled front-end using HTML and CSS</li>
-                    </ul>
-                    </div>
-                </section>
-                <section id="project4">
-                    <a href="https://calm-candle-shop.onrender.com/" target="_blank" rel="noopener noreferrer"><img className="project-image" src="src/assets/Screenshot 2023-08-04 at 5.09.14 PM (2).png"></img></a>
-                    <h4><a href="https://calm-candle-shop.onrender.com/" className="project-title" target="_blank" rel="noopener noreferrer">Calm Candle Shop</a></h4>
-                    <div className="project-description">
-                        <p>Full-Stack MEAN Application</p>
-                        <p>HTML, CSS, TypeScript, Angular, MongoDB, Mongoose    08/2023</p>
-                    <ul>
-                        <li>Built frontend in Angular and TypeScript</li>
-                        <li>Backend API built using MongoDB, Mongoose, Express, and Node JS</li>
                     </ul>
                     </div>
                 </section>
--- Margaret-Camilletti-Portfolio/src/components/Resume.jsx
+++ Margaret-Camilletti-Portfolio/src/components/Resume.jsx
@@ -15,7 +15,7 @@ Languages |</strong> JavaScript, HTML, CSS, Python, TypeScript, Go<br></br>
 <strong>PROFESSIONAL EXPERIENCE<br></br><br></br>
 
 <strong></strong>Backend Developer | Revent Solutions | Remote 11/2023-Present</strong><br></br>
-<ul><li>Collaborated with the backend team at Revent Solutions to develop robust, secure, and scalable web applications, leveraging the MERN stack (MongoDB, Express.js, React, Node.js) for full-stack development efficiency.</li>
+<ul><li>Collaborated with the backend team at Revent Solutions to develop robust, secure, and scalable web applications using Node.js framework.</li>
 <li>Engineered tailored solutions by analyzing insightful data, integrating Oracle databases to enhance data management and application performance.</li>
 <li>Actively participated in a dynamic team of developers to architect and implement the backend API for WalletWize, a personal finance application, ensuring seamless data integration and user experience.</li>
 <li>Employed Agile and Scrum methodologies to streamline project management, enhance team collaboration, and ensure timely delivery of high-quality software features.</li>
