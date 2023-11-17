81f8ca3109f7b2cd9563c0743754b33cdea2ca08
Margaret Camilletti <mhcamilletti@gmail.com>
2023-11-17 09:49:31 -0500

    fixed typo in linkedin link

--- Margaret-Camilletti-Portfolio/src/components/Header.jsx
+++ Margaret-Camilletti-Portfolio/src/components/Header.jsx
@@ -46,7 +46,7 @@ const Header = () => {
       </div>
       <div className="right-side">
       <a target="_blank" rel="noopener noreferrer" href="https://github.com/mcamilletti1"><img height="30px" width="30px" className="icon" src="src/assets/pngaaa.com-94893.png" alt="GitHub"></img></a>
-      <a target="_blank" rel="noopener noreferrer" href="https://linkedin.com/in/margaretcamilletti"><img height="30px" width="30px" className="icon" src="src/assets/kisspng-united-states-hilton-hotels-resorts-organization-linkedin-white-5b39721a0b09e5.9500994915304914180452.png" alt="LinkedIn"></img></a>
+      <a target="_blank" rel="noopener noreferrer" href="https://linkedin.com/in/margaretcamilletti/"><img height="30px" width="30px" className="icon" src="src/assets/kisspng-united-states-hilton-hotels-resorts-organization-linkedin-white-5b39721a0b09e5.9500994915304914180452.png" alt="LinkedIn"></img></a>
         <div className={`hamburger-menu ${showMenu ? 'active' : ''}`} 
         onClick={handleMenuToggle}
         onKeyDown={handleMenuKeyDown}
