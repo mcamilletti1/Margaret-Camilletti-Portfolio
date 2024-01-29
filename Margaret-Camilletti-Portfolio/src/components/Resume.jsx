b0553ece4701adef771b65085abe1d7c05fd5fd4
Margaret Camilletti <mhcamilletti@gmail.com>
2024-01-29 13:53:56 -0500

    updated resume section to add new position

--- Margaret-Camilletti-Portfolio/src/components/Resume.jsx
+++ Margaret-Camilletti-Portfolio/src/components/Resume.jsx
@@ -14,6 +14,16 @@ Languages |</strong> JavaScript, HTML, CSS, Python, TypeScript, Go<br></br>
 
 <strong>PROFESSIONAL EXPERIENCE<br></br><br></br>
 
+<strong>Frontend Developer | 99 Yards | Remote | 01/2024-Present</strong><br></br>
+<ul>
+  <li>Designed and implemented visually stunning user interfaces, delivering an engaging and seamless experience for our app users.</li>
+  <li>Collaborated closely with the design and business teams to translate their visions into high-performance, responsive, and user-friendly mobile interfaces.</li>
+  <li>Leveraged the latest React Native technologies to optimize app performance, ensuring fast load times and smooth navigation.</li>
+  <li>Actively participated in code reviews and provided valuable feedback to maintain code quality and consistency across the development team.</li>
+  <li>Collaborated with backend developers to seamlessly integrate frontend and backend components, ensuring a cohesive and efficient application architecture.</li>
+  <li>Played a pivotal role in user testing and feedback collection, iterating on app features and designs to enhance user satisfaction and retention.</li>
+</ul><br></br>
+
 <strong></strong>Backend Developer | Revent Solutions | Remote 11/2023-Present</strong><br></br>
 <ul><li>Collaborated with the backend team at Revent Solutions to develop robust, secure, and scalable web applications using Node.js framework.</li>
 <li>Engineered tailored solutions by analyzing insightful data, integrating Oracle databases to enhance data management and application performance.</li>
