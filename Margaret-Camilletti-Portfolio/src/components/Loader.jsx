168bd1751a11a36959b35e6f807ae57d643ed67f
Margaret Camilletti <125098092+mcamilletti1@users.noreply.github.com>
2024-04-12 15:09:55 -0400

    changed margin

--- Margaret-Camilletti-Portfolio/src/components/Loader.jsx
+++ Margaret-Camilletti-Portfolio/src/components/Loader.jsx
@@ -19,7 +19,7 @@ const CanvasLoader = () => {
           fontSize: 14,
           color: "#F1F1F1",
           fontWeight: 800,
-          marginTop: 40,
+          marginTop: 10,
         }}
       >
         {progress.toFixed(2)}%
